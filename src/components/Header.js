ad4dd5955a4fc4c217b1651a305cd81307270c74
Yasir-Mohi-ud-din <parrayyasir6@gamil.com>
2022-10-30 21:01:50 +0530

    resolved

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,13 @@ export default function Header() {
     return (
         <header className="header">
             <img 
+                
                 src="./images/troll-face.png" 
+                alt = ""
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h4 className="header--project">By Yasir-Mohi-ud-din</h4>
         </header>
     )
 }
